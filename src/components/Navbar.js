428c168b5aa353666a5b638e30902ab572127f04
Tanaka <tanakagabaza@gmail.com>
2021-11-19 12:21:43 +0000

    Creating booklist display

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,6 @@ import { BookContext } from '../contexts/BookContext';
 
 const Navbar = () => {
     const {books, author, id} = useContext(BookContext);
-    useEffect(() => {
-        console.log('SOmething broke');
-    })
     return ( 
         <div className="navbar">
             <h1>TAGABAZA READING LIST</h1>
