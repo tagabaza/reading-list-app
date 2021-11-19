6af5ed55d1ccbf5f8ce6ae33cffd64ecde0a6ddf
Tanaka <tanakagabaza@gmail.com>
2021-11-19 16:54:42 +0000

    Adding a reducer

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -2,21 +2,24 @@ import React, {useContext, useState} from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const NewBookForm = () => {
-    const { addBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        addBook(title, author);
+        dispatch({type: 'ADD_BOOK', book: {title, author}});
         setTitle('');
         setAuthor('');
+        console.log("Here");
     }
     return ( 
         <form onSubmit={ handleSubmit }>
             <label>Add new book to read</label>
-            <input type="text" placeholder="Book title" onChange={(e) => setTitle(e.target.value)} required />
-            <input type="text" placeholder="Book author" onChange={(e) => setAuthor(e.target.value)} required />
+            <input type="text" placeholder="Book title" 
+                onChange={(e) => setTitle(e.target.value)} required />
+            <input type="text" placeholder="Book author" 
+                onChange={(e) => setAuthor(e.target.value)} required />
             <input type="submit" value="Add Book" />
         </form>
      );
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,39 +1,12 @@
-import React, {createContext, useState} from 'react';
-import {v4 as uuidv4} from 'uuid';
+import React, {createContext, useReducer} from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
-// const BookContextProvider = (props) => {
-//     const [ books, setBooks ] = useState([
-//         {title: 'Orphan X', author: 'Greg Hurwitz', id: 1 },
-//         {title: 'Prodigal Son', author: 'Hurwitz', id: 2 }
-//     ]);
-//     const addBook = (title, author) => {
-//         setBooks([...books, {title, author, id: uuidv4() }]);
-//     };
-//     const removeBooks = (id) => {
-//         setBooks(books.filter(book => book.id !== id));
-//     };
-//     return (  
-//         <BookContext.Provider value={{books, addBook, removeBooks}}>
-//             { props.children };
-//         </BookContext.Provider>
-//     )
-// }
-
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'Orphan X', author: 'Greg Hurwitz', id: 1 },
-        {title: 'Prodigal Son', author: 'Hurwitz', id: 2 }
-    ]);
-    const addBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuidv4() }]);
-    };
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id));
-    };
+    const [ books, dispatch ] = useReducer(bookReducer, []);
     return(
-        <BookContext.Provider value={{books, addBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             { props.children }
         </BookContext.Provider>
     )
